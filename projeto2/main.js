6636cdae0a7be9d1ad3dd884649295baf520eb74
mailsonpuc <133193122+mailsonpuc@users.noreply.github.com>
2024-02-23 15:59:44 -0300

    Update main.js

--- projeto2/main.js
+++ projeto2/main.js
@@ -3,24 +3,24 @@ import { encerramento1, encerramento2 ,encerramento3, encerramento4, encerrament
 
 
 //end 1
-document.getElementById("encerramento1").innerHTML=`<s>Faltam <i class="#e040fb purple accent-2">${encerramento1(new Date('2023,09,03'), new Date())} DIAS</i> para Etapa 1:
+document.getElementById("encerramento1").innerHTML=`Faltam <i class="#e040fb purple accent-2">${encerramento1(new Date('2024,03,24'), new Date())} DIAS</i> para Etapa 1:
 
-Análise e especificação do problema</s>`
+Análise e especificação do problema`
 
 //end 2
-document.getElementById("encerramento2").innerHTML=`<s>Faltam <i class="#e040fb purple accent-2">${encerramento1(new Date('2023,09,24'), new Date())} DIAS</i> para Etapa 2:
+document.getElementById("encerramento2").innerHTML=`Faltam <i class="#e040fb purple accent-2">${encerramento1(new Date('2024,04,24'), new Date())} DIAS</i> para Etapa 2:
 
-Projeto dos requisitos e de artefatos do sistema</s>`
+Projeto dos requisitos e de artefatos do sistema`
 
 //end 3
-document.getElementById("encerramento3").innerHTML=`<s>Faltam <i class="#e040fb purple accent-2">${encerramento1(new Date('2023,10,29'), new Date())} DIAS</i> para Etapa 3:
+document.getElementById("encerramento3").innerHTML=`Faltam <i class="#e040fb purple accent-2">${encerramento1(new Date('2024,02,06'), new Date())} DIAS</i> para Etapa 3:
 
-Desenvolvimento da solução (Parte 1</s>)`
+Desenvolvimento da solução (Parte 1)`
 
 //end 4
-document.getElementById("encerramento4").innerHTML=`<s>Faltam <i class="#e040fb purple accent-2">${encerramento1(new Date('2023,11,26'), new Date())} DIAS</i> para Etapa 4:
+document.getElementById("encerramento4").innerHTML=`Faltam <i class="#e040fb purple accent-2">${encerramento1(new Date('2024,06,24'), new Date())} DIAS</i> para Etapa 4:
 
-Desenvolvimento da solução (Parte 2)</s>`
+Desenvolvimento da solução (Parte 2)`
 
 //end 5
 document.getElementById("encerramento5").innerHTML=`Faltam <i class="#e040fb purple accent-2">${encerramento1(new Date('2023,12,10'), new Date())} DIAS</i> para Etapa 5:
