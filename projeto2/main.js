aeac762424cd2e90197655520e81707cc6cb4a8f
mailsonpuc <133193122+mailsonpuc@users.noreply.github.com>
2024-02-23 16:18:06 -0300

    Update main.js
    atual -2024

--- projeto2/main.js
+++ projeto2/main.js
@@ -2,23 +2,23 @@ import { encerramento1, encerramento2 ,encerramento3, encerramento4, encerrament
 
 
 
-//end 1
+//end 1 dia/mes/ano
 document.getElementById("encerramento1").innerHTML=`Faltam <i class="#e040fb purple accent-2">${encerramento1(new Date('04,03,2024'), new Date())} DIAS</i> para Etapa 1:
 
 Análise e especificação do problema`
 
 //end 2
-document.getElementById("encerramento2").innerHTML=`Faltam <i class="#e040fb purple accent-2">${encerramento1(new Date('2024,04,24'), new Date())} DIAS</i> para Etapa 2:
+document.getElementById("encerramento2").innerHTML=`Faltam <i class="#e040fb purple accent-2">${encerramento1(new Date('14,04,2024'), new Date())} DIAS</i> para Etapa 2:
 
 Projeto dos requisitos e de artefatos do sistema`
 
 //end 3
-document.getElementById("encerramento3").innerHTML=`Faltam <i class="#e040fb purple accent-2">${encerramento1(new Date('2024,02,06'), new Date())} DIAS</i> para Etapa 3:
+document.getElementById("encerramento3").innerHTML=`Faltam <i class="#e040fb purple accent-2">${encerramento1(new Date('02,06,2024'), new Date())} DIAS</i> para Etapa 3:
 
 Desenvolvimento da solução (Parte 1)`
 
 //end 4
-document.getElementById("encerramento4").innerHTML=`Faltam <i class="#e040fb purple accent-2">${encerramento1(new Date('2024,06,24'), new Date())} DIAS</i> para Etapa 4:
+document.getElementById("encerramento4").innerHTML=`Faltam <i class="#e040fb purple accent-2">${encerramento1(new Date('23,06,2024'), new Date())} DIAS</i> para Etapa 4:
 
 Desenvolvimento da solução (Parte 2)`
 
