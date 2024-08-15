6c65fd9e03ea858d752f94f13a13235157d21197
mailsonpuc <mailson.costa@sga.pucminas.br>
2024-08-14 22:12:24 -0300

    xp inc

--- projeto2/encerramento.js
+++ projeto2/encerramento.js
@@ -63,3 +63,17 @@ export function encerramento5(date_1, date_2){
     return TotalDays;
 
 }
+
+
+
+
+
+
+ export function encerramento6(date_1, date_2){
+    let difference = date_1.getTime() - date_2.getTime()
+
+    let TotalDays = Math.ceil(difference / (1000 * 3600 * 24))
+
+    return TotalDays;
+
+}
