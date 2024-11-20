033426b26d8e60cdaf58602ec6f511dca23dcdaa
mailsonpuc <mailson.costa@sga.pucminas.br>
2024-11-20 20:54:21 -0300

    Update main.js

--- projeto2/main.js
+++ projeto2/main.js
@@ -18,4 +18,4 @@ document.getElementById("encerramento4").innerHTML=`Faltam <i class="#e040fb pur
 document.getElementById("encerramento5").innerHTML=`Faltam <i class="#e040fb purple accent-2">${encerramento1(new Date('2024,11,25'), new Date())} DIAS</i> Tarefa 5 (Etapa 5)`
 
 //end 6
-document.getElementById("encerramento6").innerHTML=`Faltam <i class="#e040fb purple accent-2">${encerramento1(new Date('2024,11,17'), new Date())} DIAS</i> XP Inc. - Full Stack Developer dio.me`
+document.getElementById("encerramento6").innerHTML=`<del>Faltam <i class="#e040fb purple accent-2">${encerramento1(new Date('2024,11,17'), new Date())} DIAS</i> XP Inc. - Full Stack Developer dio.me</del>`
