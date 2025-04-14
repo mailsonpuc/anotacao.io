372c939bba90b9e4bbaf883eea56af8ba2b6faaa
mailsonpuc <mailson.costa@sga.pucminas.br>
2025-04-14 15:23:09 -0300

    Update main.js

--- projeto2/main.js
+++ projeto2/main.js
@@ -3,19 +3,26 @@ import { encerramento1, encerramento2 ,encerramento3, encerramento4, encerrament
 
 
 //end 1 dia/mes/ano
-document.getElementById("encerramento1").innerHTML=`<del>Faltam <i class="#e040fb purple accent-2">${encerramento1(new Date('2024,08,26'), new Date())} DIAS</i> Tarefa 1 (Etapa 1)</del>`
+document.getElementById("encerramento1").innerHTML=`<del>Faltam <i class="#e040fb purple accent-2">${encerramento1(new Date('2025,03,09'), new Date())} DIAS</i> Tarefa 1 (Etapa 1)</del>`
 
 //end 2
-document.getElementById("encerramento2").innerHTML=`<del>Faltam <i class="#e040fb purple accent-2">${encerramento1(new Date('2024,09,23'), new Date())} DIAS</i> Tarefa 2 (Etapa 2)</del>`
+document.getElementById("encerramento2").innerHTML=`<del>Faltam <i class="#e040fb purple accent-2">${encerramento1(new Date('2025,04,20'), new Date())} DIAS</i> Tarefa 2 (Etapa 2)</del>`
 
 //end 3
-document.getElementById("encerramento3").innerHTML=`<del>Faltam <i class="#e040fb purple accent-2">${encerramento1(new Date('2024,10,21'), new Date())} DIAS</i> Tarefa 3 (Etapa 3)</del>`
+document.getElementById("encerramento3").innerHTML=`<del>Faltam <i class="#e040fb purple accent-2">${encerramento1(new Date('2025,05,25'), new Date())} DIAS</i> Tarefa 3 (Etapa 3)</del>`
 
 //end 4
-document.getElementById("encerramento4").innerHTML=`Faltam <i class="#e040fb purple accent-2">${encerramento1(new Date('2024,11,18'), new Date())} DIAS</i> Tarefa 4 (Etapa 4)`
+document.getElementById("encerramento4").innerHTML=`Faltam <i class="#e040fb purple accent-2">${encerramento1(new Date('2025,06,25'), new Date())} DIAS</i> Tarefa 4 (Etapa 4)`
 
 //end 5
-document.getElementById("encerramento5").innerHTML=`Faltam <i class="#e040fb purple accent-2">${encerramento1(new Date('2024,11,25'), new Date())} DIAS</i> Tarefa 5 (Etapa 5)`
+document.getElementById("encerramento5").innerHTML=`Faltam <i class="#e040fb purple accent-2">${encerramento1(new Date('2025,05,02'), new Date())} DIAS</i> 1ª Avaliação`
 
 //end 6
-document.getElementById("encerramento6").innerHTML=`<del>Faltam <i class="#e040fb purple accent-2">${encerramento1(new Date('2024,11,17'), new Date())} DIAS</i> XP Inc. - Full Stack Developer dio.me</del>`
+
+document.getElementById("encerramento6").innerHTML=`Faltam <i class="#e040fb purple accent-2">${encerramento1(new Date('2025,06,06'), new Date())} DIAS</i> 2ª Avaliação`
+
+
+document.getElementById("encerramento7").innerHTML=`Faltam <i class="#e040fb purple accent-2">${encerramento1(new Date('2025,06,15'), new Date())} DIAS</i> 3ª Avaliação`
+
+
+document.getElementById("encerramento8").innerHTML=`Faltam <i class="#e040fb purple accent-2">${encerramento1(new Date('2025,06,29'), new Date())} DIAS</i> Re-Avaliação`
